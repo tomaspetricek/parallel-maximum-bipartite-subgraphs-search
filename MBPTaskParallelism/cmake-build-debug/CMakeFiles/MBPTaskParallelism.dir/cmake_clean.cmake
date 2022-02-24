87260a3bb645100d7261835416791c0495399ecb
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2022-02-24 17:33:29 +0100

    Add task parallelism

---
+++ MBPTaskParallelism/cmake-build-debug/CMakeFiles/MBPTaskParallelism.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MBPTaskParallelism.dir/main.cpp.o"
+  "MBPTaskParallelism"
+  "MBPTaskParallelism.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MBPTaskParallelism.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MBPTaskParallelism/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+    int p = omp_get_num_threads();
+
+    std::cout << "N cores: " << p << std::endl;
+
+    #pragma omp parallel
+    {
+        #pragma omp critical
+        std::cout << "Hello World!" << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
